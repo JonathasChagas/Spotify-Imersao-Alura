7bb255aa7721fdc395763e0bbf7163e4518a2eb3
JonathasChagas <jonathas252423@gmail.com>
2024-01-26 16:21:15 -0300

    🧨 Artists API

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
 const searchInput = document.getElementById ('search-input');
-const resultArtists = document.getElementById ('result-artist');
+const resultArtist = document.getElementById ('result-artist');
 const resultPlaylist = document.getElementById ('result-playlists')
 
 function requestApi(searchTerm) {
     fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
         .then((response) => response.json())
-        .then((results) => displayResults(results))
+        .then((result) => displayResults(result))
 }
 
 function displayResults(results) {
     resultPlaylist.classList.add ('hidden');
-    const artistName= document.getElementById('artist-name');
-    const artistImage = document.getElementById ('artist-img');
+    const artistName = document.getElementById("artist-name");
+    const artistImage = document.getElementById ("artist-img");
 
     results.forEach((element) => {
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
     });
 
-    resultArtists.classList.remove('hidden');
+    resultArtist.classList.remove('hidden');
 }
 
 document.addEventListener ('input', function() {
     const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === "") {
         resultPlaylist.classList.remove ('hidden');
-        resultArtists.classList.add('hidden');
+        resultArtist.classList.add('hidden');
         return;
     }
     requestApi (searchTerm);
